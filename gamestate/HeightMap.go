329d2080fee14e3b6fe8772871a517884ac4bba5
Arne Döring <arne.doering@rwth-aachen.de>
2014-02-27 22:46:41 +0100

    irgendwas

--- gamestate/HeightMap.go
+++ gamestate/HeightMap.go
@@ -36,43 +36,18 @@ func NewHeightMap(w, h int) (out *HeightMap) {
 	return
 }
 
-func (this *HeightMap) MaxLod() int {
-	a := helpers.Log2(uint64(this.W))
-	b := helpers.Log2(uint64(this.H))
-	if a < b {
-		return int(a)
-	} else {
-		return int(b)
-	}
+var gauss_factor = 1 / math.Sqrt(2*math.Pi)
+
+func Gauss(x float32) float32 {
+	return float32(math.Exp(float64(-x*x/2)) * gauss_factor)
 }
 
-func (this *HeightMap) BoundsLod(x, y, lod int) (min, max float32) {
-	x = x & (this.W - 1)
-	y = y & (this.H - 1)
-	w := this.W
-	h := this.H
+func Gauss2f(v mgl.Vec2f) mgl.Vec2f {
+	return mgl.Vec2f{Gauss(v[0]), Gauss(v[1])}
+}
 
-	if lod > this.MaxLod() {
-		panic("lod too high")
-	}
+func Bump(center mgl.Vec2f, height float32) {
 
-	if lod == 0 {
-		return this.Get(x, y), this.Get(x, y)
-	} else {
-		x, y = x>>1, y>>1
-	}
-
-	offset := 0
-	for ; lod > 0; lod -= 1 {
-		offset += x * y
-		x, y = x>>1, y>>1
-		w, h = w>>1, h>>1
-		lod -= 1
-	}
-
-	min = this.MinTree[offset+w*y+x]
-	max = this.MaxTree[offset+w*y+x]
-	return
 }
 
 func (this *HeightMap) Get(x, y int) float32 {
@@ -138,33 +113,6 @@ func (m *HeightMap) Normal(x int, y int) mgl.Vec3f {
 
 	return n1.Add(n2).Add(n3).Add(n4).Normalize()
 }
-
-/*
-func (m *HeightMap) MinHm() (minHM *HeightMap, maxHM *HeightMap) {
-	out = NewHeightMap(m.W/2, m.H/2)
-	for x := 0; x < out.W; x++ {
-		for y := 0; y < out.H; y++ {
-			v := [4]float32{
-				m.Get(2*x, 2*y),
-				m.Get(2*x, 2*y+1),
-				m.Get(2*x+1, 2*y),
-				m.Get(2*x+1, 2*y+1),
-			}
-			min, max := v[0], v[0]
-			for i := 1; i < 4; i++ {
-				if v[i] < min {
-					min = v[i]
-				}
-				if v[i] > max {
-					max = v[i]
-				}
-			}
-			minHm.Set(x, y, min)
-			maxHm.Set(x, y, max)
-		}
-	}
-}
-*/
 
 func (m *HeightMap) Normalf(x float32, y float32) (n mgl.Vec3f) {
 	x0 := int(math.Floor(float64(x)))
